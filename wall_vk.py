d478cc8f42d8391292272cd0c4fba9983bf3bcfb
Proskurova <proskurovalyuda@gmail.com>
2023-01-21 21:35:40 +0300

    I divided the functions into thematic files

--- wall_vk.py
+++ wall_vk.py
@@ -1,20 +1,16 @@
-import requests
+from requests_vk import RequestsVK
+import config as con
 
 
-def make_request(method: str, params: dict) -> dict:
-    response = requests.get('https://api.vk.com/method/' + method, params=params)
-    data = response.json()
-    return data
+res = RequestsVK(con.TOKEN, con.VERSION)
 
 
-def get_1000_posts(token: str, version: float, domain: str, count: int, offset=0) -> list:
+def get_1000_posts(domain: str, count: int, offset=0) -> list:
     '''
     1000 first posts from the community wall.
-    :param token: user access key VK API
-    :param version: API Versions VK
-    :param domain: The short address of the user or community.
-    :param count: The number of records to be retrieved. Maximum: 100
-    :param offset: The offset required to select a specific subset of record.
+        :param domain: The short address of the user or community.
+        :param count: The number of records to be retrieved. Maximum: 100
+        :param offset: The offset required to select a specific subset of record.
     :return: The list of dictionaries with data for each posts
     '''
 
@@ -23,34 +19,55 @@ def get_1000_posts(token: str, version: float, domain: str, count: int, offset=0
     while offset < 1000:
         method = "wall.get"
         params = {
-            'access_token': token,
-            'v': version,
+            'access_token': str,
+            'v': float,
             'domain': domain,
             'count': count,
             'offset': offset
         }
-        data = make_request(method, params)['response']['items']
+        data = res.make_request(method, params)['response']['items']
         offset += 100
         all_posts.extend(data)
     return all_posts
 
 
-def get_post_id(token: str, version: float, posts: str, extended=0) -> list:
+def get_post_id(posts: str, extended=0, fields=None) -> list:
     '''
     Returns a list of records from the walls of users or communities by their IDs.
-    :param token: user access key VK API
-    :param version: API Versions VK
-    :param posts: The string is of this format: The owner_id and the post_id going through the underscore sign\
-    and it is comma-separated ("93388_21539,-1_20904")
-    :param extended: if the value is 1, additional profiles and groups fields will be returned in the response
+        :param posts: The string is of this format: The owner_id and the post_id going through the underscore sign\
+        and it is comma-separated ("93388_21539,-1_20904")
+        :param extended: if the value is 1, additional profiles and groups fields will be returned in the response
+        :param fields: List of additional fields for profiles and groups to be returned.Note that this parameter \
+        is taken into account only when extended=1.
     :return: Data from the wall is about the requested posts
     '''
     method = "wall.getById"
     params = {
-        'access_token': token,
-        'v': version,
+        'access_token': str,
+        'v': float,
         'posts': posts,
-        'extended': extended
+        'extended': extended,
+        'fields': fields
     }
 
-    return make_request(method, params)['response']
+    return res.make_request(method, params)['response']
+
+
+# def create_record_wall(token_community: str, version: float, owner_id: int, friends_only=0, from_group=0,
+#                        message='', attachments=None):
+#     method = "wall.post"
+#     params = {
+#         'access_token': token_community,
+#         'v': version,
+#         'owner_id': owner_id,
+#         'friends_only': friends_only,
+#         'from_group': from_group,
+#         'message': message,
+#         'attachments': attachments
+#
+#     }
+#     return make_request(method, params)
+
+
+
+
